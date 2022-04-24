3d7bedb6d54a1d08d4555c4c3023f4bb990868af
Okens <Ng.Eric314@gmail.com>
2022-04-24 16:59:45 -0700

    Adding get all products route.

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,15 +4,29 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  console.log(`${req.method}: ${req.baseUrl}`);
+  try {
+    const productData = await Product.findAll({
+      include: [
+        {model: Tag},
+        {model: Category}
+      ]
+    })
+    res.status(200).json(productData);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  console.log(`${req.method}: ${req.baseUrl}`);
+  
 });
 
 // create new product
