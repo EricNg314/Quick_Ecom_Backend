16013b4b6c76caadf264c08e5b5562b1e93b0eb4
Okens <Ng.Eric314@gmail.com>
2022-04-24 01:10:30 -0700

    Fixing foreignKey many to many issue.

--- models/index.js
+++ models/index.js
@@ -17,13 +17,15 @@ Category.hasMany(Product, {
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: ProductTag,
-  foreignKey: 'tag_id'
+  as: 'tag_product',
+  foreignKey: 'product_id'
 })
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: ProductTag,
-  foreignKey: 'product_id'
+  as: 'tag_product',
+  foreignKey: 'tag_id'
 })
 
 module.exports = {
